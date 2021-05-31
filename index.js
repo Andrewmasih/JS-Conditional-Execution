f6b8a4c8244c6b37c086a3b3471e906695633950
Andrew Masih <armasih@outlook.com>
2021-05-31 18:46:13 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+/* conditional execution perform different actions based on the result of the condition,you can use it by using the 'if' or 'else' or 'switch' statement. */
+
+
+/* conditions are like a cross road, you can go left or right, either true or false, this will control the flow of the programme, depending on the conditions, some code block will only run oif the condtions are true! what ever is false will be skipped and not run!  */
+
+
+
+// try changing the sky to 'gray'
+let sky = "blue";
+
+
+
+
+if (sky == "blue") {
+
+  // sky is blue, meaning the condition is true the code will run
+
+  console.log("nice weather");
+
+} else {
+
+  console.log("not nice weather");
+
+
+};
